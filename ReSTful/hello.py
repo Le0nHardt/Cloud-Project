16db5088864c43a0c4a0af0d62ba046100fba544
ZhenweiWang <wangzhenweiau@gmail.com>
2016-05-10 17:54:21 +1000

    fix bugs

--- ReSTful/hello.py
+++ ReSTful/hello.py
@@ -216,7 +216,7 @@ def twitter_sydney():
 #-------------------------------------------------------------------------------
 
 @app.route('/aurin/rate_in_melbourne',methods = ['GET'])
-def aurin_melbourne):
+def aurin_melbourne():
     melboure_data_aurin = {
         'unemployment population':  176645,
         'employment population':  1971396,
